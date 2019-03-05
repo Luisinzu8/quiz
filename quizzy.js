35c34e73a01f8f37f21b89cf1fb0f9f26edb8950
Luis Inzunza <luisinzu08@gmail.com>
2019-03-05 12:00:59 -0800

    timer function

--- quizzy.js
+++ quizzy.js
@@ -9,28 +9,14 @@ function myFunction() {
   document.getElementById("hello").innerHTML = txt;
 }
 
-var timeoutHandle;
-    function countdown(minutes, seconds) {
-        function tick() {
-            var counter = document.getElementById("timer");
-            counter.innerHTML =
-                minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-            seconds--;
-            if (seconds >= 0) {
-                timeoutHandle = setTimeout(tick, 1000);
-            } else {
-                if (minutes >= 1) {
-                    // countdown(mins-1);   never reach “00″
-                    setTimeout(function () {
-                        countdown(minutes - 1, 59);
-                    }, 1000);
-                }
-            }
-        }
-        tick();
-    }
+var myVar = setInterval(function() {
+  myTimer()
+}, 1000);
+var d = 10;
 
-    countdown(1, 30);
+function myTimer() {
+  document.getElementById("timer").innerHTML = d--;
+}
 
 (function() {
   function buildQuiz() {
